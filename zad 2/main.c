a90ac06324f04f3596ce10f387d2b0ee9726fe13
skrampyy <79580383+skrampyy@users.noreply.github.com>
2024-10-26 12:20:09 +0200

    Update main.c
    -comments

--- zad 2/main.c
+++ zad 2/main.c
@@ -11,7 +11,6 @@ int chooseOperation(int intA, int intB, float floatA, float floatB, int isFloat)
     printf("1: Dodawanie\n");
     printf("2: Odejmowanie\n");
     printf("3: Mnozenie\n");
-    //ponizej prosto z zadan na labach :)
     printf("%s\n", ((intB != 0 && !isFloat) || (floatB != 0 && isFloat)) ? "4: Dzielenie":"4: Niedostepne - nie mozna dzielic przez zero");
     if (!isFloat) {printf("5: %s\n",(intB!=0 ? "Dzielenie" : "Niedostepne - nie mozna dzielic przez 0"));} else {printf("5: Niedostepne - nie istnieje modulo dla liczb zmiennoprzecinkowych\n");}
 
